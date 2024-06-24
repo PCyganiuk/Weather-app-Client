736c55650173fceb2b512b6ea6fec1da8b196aa8
PCyganiuk <pcyganiuk@gmail.com>
2024-06-24 16:56:53 +0200

    initial commit

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './Navbar';
 import Chart from './Chart';
-import Features from './Features';
 import Login from './Login';
 import WeatherForecast from './WeatherForecast';
 
