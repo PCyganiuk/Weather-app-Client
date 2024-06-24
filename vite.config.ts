cd966faf56d3e3755f5cde8288fd471edba16803
PCyganiuk <pcyganiuk@gmail.com>
2024-06-24 23:47:26 +0200

    pretty please work

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 import commonjs from 'vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react(), commonjs() ],
+
+  build: {
+    commonjsOptions: {transformMixedEsModules: true}
+  }
 })
